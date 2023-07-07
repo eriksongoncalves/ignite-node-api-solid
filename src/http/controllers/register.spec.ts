5eae641fc72fdb00e702f28d4c5f7a35b4bc3dcc
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2023-07-07 14:34:03 -0300

    test: register controller

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,24 @@
-import { describe, it } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+import { app } from '@/app'
 
 describe('Register controller', () => {
-  it('ok', () => {})
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('it should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456'
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
